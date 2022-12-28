112c90a5f8a6d25894c46e077fb48c93963b7c48
girisken07 <ibrahimgirisken@hotmail.com>
2022-12-28 23:15:33 +0300

    28-12-2022

---
+++ ETicaretAPI.Persistence/ServiceRegistrations.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistrations
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+          
+        }
+    }
+}
