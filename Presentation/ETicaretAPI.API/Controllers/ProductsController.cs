322f95473171f15ff5f7d95c893e2d8b2849663d
ibrahimgirisken <ibrahimgirisken@hotmail.com>
2023-02-04 19:41:50 +0300

    04-02-2023

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Application.RequestParameters;
 using ETicaretAPI.Application.ViewModels.Products;
 using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
@@ -23,9 +24,10 @@ namespace ETicaretAPI.API.Controllers
 
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get([FromQuery]Pagination pagination)
         {
-            return Ok(_productReadRepository.GetAll(false).Select(p => new
+            var totalCount = _productReadRepository.GetAll().Count();
+           var products= _productReadRepository.GetAll(false).Skip(pagination.Page * pagination.Size).Take(pagination.Size).Select(p => new
             {
                 p.Id,
                 p.Name,
@@ -33,7 +35,12 @@ namespace ETicaretAPI.API.Controllers
                 p.Price,
                 p.CreatedDate,
                 p.UpdateDate
-            }));
+            });
+            return Ok(new
+            {
+                totalCount,
+                products
+            });
        
         }
 
